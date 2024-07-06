843a2a8e356e6d69a514eafbe9b77efd1d4a6460
Mahendra <mudiyammahendra123@gmail.com>
2024-07-06 17:55:02 +0530

    Added swap two numbers program

---
+++ src/test/java/Program/swaptwoprogram.java
@@ -0,0 +1,16 @@
+package Program;
+
+public class swaptwoprogram {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		a=a+b;
+
+		b=a-b;
+		a=a-b;
+		System.out.println("a="+a +"b="+b);
+
+	}
+
+}
